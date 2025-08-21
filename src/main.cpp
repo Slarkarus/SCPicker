4473dda41c3637423e885bce3a4d6ef4cf1f0d7f
Евгений Мартьянов <martianov.em@phystech.edu>
2025-08-21 23:39:47 +0300

    Add random

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,15 @@
 #include "viewer.hpp"
 #include "controller.hpp"
 
+#include <cstdlib>
+#include <ctime>
+
 int main() {
+    std::srand(std::time(nullptr));
+
     Controller* controller = new Controller();
     Viewer* viewer = new Viewer();
-    Gamedata* gamedata = new Gamedata("original_game_map.scpac");
+    Gamedata* gamedata = new Gamedata("test_map.scpac");
 
     Engine* engine = Engine::get_instance(controller, gamedata, viewer);
 
