a912a4109a27929fe0a7cfd6a3710031f95409d5
Slarkarus <slarkarus@gmail.com>
2025-08-21 18:13:01 +0300

    Add events.hpp

---
+++ include/events.hpp
@@ -0,0 +1,16 @@
+#ifndef EVENTS_HPP
+#define EVENTS_HPP
+
+enum class Event
+{
+    Up,
+    Down,
+    Left,
+    Right,
+    Space,
+    Enter,
+    Escape,
+    CountOfEvents
+};
+
+#endif // EVENTS_HPP
