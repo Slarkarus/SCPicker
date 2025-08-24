9c1fc038b6003627bdda3db926fb44ec2ec579dd
Slarkarus <slarkarus@gmail.com>
2025-08-24 19:17:20 +0300

    Implements functions for set_path

--- src/entity/scp_939.cpp
+++ src/entity/scp_939.cpp
@@ -2,7 +2,14 @@
 #include "entity/scp_939.hpp"
 #include "entity/player.hpp"
 #include "constants.hpp"
+
 #include <cmath>
+#include <cstdlib>
+#include <algorithm>
+
+// TODO: replace std::vector<std::pair<double, double>> with c++ using like
+// using Point = std::pair<double, double>;
+// using VectorPoints = std::vector<Point>;
 
 bool player_is_near(ent::SCP_939 *scp, Gamedata *gamedata)
 {
@@ -99,11 +106,63 @@ std::vector<std::pair<double, double>> find_path_centers(std::pair<double, doubl
     return path_centers;
 }
 
-void set_path_to_player(ent::SCP_939 *scp, Gamedata *gamedata) {}
+std::vector<std::pair<double, double>> get_scp_939_path(std::pair<double, double> a, std::pair<double, double> b, Gamedata *gamedata)
+{
+    std::vector<std::pair<double, double>> center_path = find_path_centers(a, b, gamedata);
+    std::vector<std::pair<double, double>> true_path;
 
-void set_random_path(ent::SCP_939 *scp, Gamedata *gamedata) {}
+    // Поочерёдно, для каждой пары точек, добавляем все точки, которые пройдёт scp_939 кроме конечной точки, чтобы избежать дублирования точек
+    for (int i = 0; i + 1 < center_path.size(); ++i)
+    {
+        std::pair<double, double> first_point = center_path[i];
+        std::pair<double, double> second_point = center_path[i + 1];
 
-void set_pos(ent::SCP_939 *scp, std::vector<std::pair<double, double>> &next_path) {}
+        int num_iterations = (1.0 / SCP_939_SPEED) - 1;
+        if (first_point.first != second_point.first)
+        {
+            double dx = second_point.first - first_point.first;
+            for (int i = 0; i < num_iterations; ++i)
+            {
+                true_path.push_back({first_point.first + dx * SCP_939_SPEED * i, first_point.second});
+            }
+        }
+        else
+        {
+            double dy = second_point.second - first_point.second;
+            for (int i = 0; i < num_iterations; ++i)
+            {
+                true_path.push_back({first_point.first, first_point.second + dy * SCP_939_SPEED * i});
+            }
+        }
+    }
+    true_path.push_back(center_path.back());
+    std::reverse(true_path.begin(), true_path.end());
+    return true_path;
+}
+
+void set_path_to_player(ent::SCP_939 *scp, Gamedata *gamedata)
+{
+    scp->next_path_ = get_scp_939_path(scp->get_pos(), gamedata->get_player()->get_pos(), gamedata);
+}
+
+void set_random_path(ent::SCP_939 *scp, Gamedata *gamedata)
+{
+    std::vector<std::vector<Tile>> map = gamedata->get_map();
+    int width = map[0].size();
+    int height = map.size();
+    int x = -1, y = -1;
+    while (x == -1 || y == -1 || (map[y][x] != Tile::Empty || map[y][x] != Tile::Exit))
+    {
+        x = std::rand() % width;
+        y = std::rand() % height;
+    }
+
+    scp->next_path_ = get_scp_939_path(scp->get_pos(), {(double)x + 0.5, (double)y + 0.5}, gamedata);
+}
+
+void set_pos(ent::SCP_939 *scp, std::vector<std::pair<double, double>> &next_path) {
+    
+}
 
 void ent::SCP_939::step(Gamedata *gamedata)
 {
