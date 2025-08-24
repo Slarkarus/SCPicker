4a1c48574882ada5cf2f8dbdc4b33a4911cee509
Евгений Мартьянов <martianov.em@phystech.edu>
2025-08-24 20:04:00 +0300

    Fix some bugs

--- src/entity/scp_939.cpp
+++ src/entity/scp_939.cpp
@@ -45,7 +45,8 @@ std::vector<std::pair<double, double>> find_path_centers(std::pair<double, doubl
 
     dp[start.second][start.first] = 0;
 
-    for (int i = 0; i < field_width * field_height; ++i) {
+    for (int i = 0; i < field_width * field_height; ++i)
+    {
         closest_unused = {-1, -1};
         for (int try_pos_x = 0; try_pos_x < field_width; ++try_pos_x)
         {
@@ -53,60 +54,69 @@ std::vector<std::pair<double, double>> find_path_centers(std::pair<double, doubl
             {
                 if (!used[try_pos_y][try_pos_x] &&
                     field[try_pos_y][try_pos_x] != Tile::Wall &&
-                    (closest_unused.first == -1 
-                    || dp[try_pos_y][try_pos_x] < dp[closest_unused.second][closest_unused.first])) {
-                        closest_unused = {try_pos_x, try_pos_y};    
-                    }
+                    (closest_unused.first == -1 || dp[try_pos_y][try_pos_x] < dp[closest_unused.second][closest_unused.first]))
+                {
+                    closest_unused = {try_pos_x, try_pos_y};
+                }
             }
         }
-        
+
         closest_len = dp[closest_unused.second][closest_unused.first];
 
-        if (closest_len >= 1e9 || closest_unused == finish) {
+        if (closest_len >= 1e9 || closest_unused == finish)
+        {
             break;
         }
 
         used[closest_unused.second][closest_unused.first] = true;
 
-        for (int y_shift = -1; y_shift <= 1; ++y_shift) {
-            for (int x_shift = -1; x_shift <= 1; ++x_shift) {
-                if (abs(x_shift) + abs(y_shift) == 1) {
-                    if (neighbour.first < 0 || neighbour.first >= field_width
-                        || neighbour.second < 0 || neighbour.second >= field_height) {
-                        continue;
-                    }
+        for (int y_shift = -1; y_shift <= 1; ++y_shift)
+        {
+            for (int x_shift = -1; x_shift <= 1; ++x_shift)
+            {
+                if (abs(x_shift) + abs(y_shift) == 1)
+                {
 
                     neighbour = {closest_unused.first + x_shift, closest_unused.second + y_shift};
-                    if (closest_len + 1 < dp[neighbour.second][neighbour.first]) {
+                    if (neighbour.first < 0 || neighbour.first >= field_width || neighbour.second < 0 || neighbour.second >= field_height)
+                    {
+                        continue;
+                    }
+                    if (closest_len + 1 < dp[neighbour.second][neighbour.first])
+                    {
                         dp[neighbour.second][neighbour.first] = closest_len + 1;
                         parent[neighbour.second][neighbour.first] = closest_unused;
                     }
                 }
             }
-        }            
+        }
     }
 
-    std::vector<std::pair<int,int>> path;
-    std::pair<int,int> current_tile = finish;
-    
-    if(parent[current_tile.second][current_tile.first] == std::pair<int, int>{-1,-1} && current_tile != start){
+    std::vector<std::pair<int, int>> path;
+    std::pair<int, int> current_tile = finish;
+
+    if (parent[current_tile.second][current_tile.first] == std::pair<int, int>{-1, -1} && current_tile != start)
+    {
         return {};
     }
 
-    while (current_tile != std::pair<int, int>{-1,-1}) {
+    while (current_tile != std::pair<int, int>{-1, -1})
+    {
         path.push_back(current_tile);
         current_tile = parent[current_tile.second][current_tile.first];
     }
     std::reverse(path.begin(), path.end());
 
-    std::vector<std::pair<double,double>> path_centers;
-    for(auto tile : path){
+    std::vector<std::pair<double, double>> path_centers;
+    for (auto tile : path)
+    {
         path_centers.push_back({tile.first + 0.5, tile.second + 0.5});
     }
+
     return path_centers;
 }
 
-std::vector<std::pair<double, double>> get_scp_939_path(std::pair<double, double> a, std::pair<double, double> b, Gamedata *gamedata)
+std::vector<std::pair<double, double>> get_scp_939_path(std::pair<double, double> a, std::pair<double, double> b, Gamedata *gamedata, double SPEED)
 {
     std::vector<std::pair<double, double>> center_path = find_path_centers(a, b, gamedata);
     std::vector<std::pair<double, double>> true_path;
@@ -117,13 +127,13 @@ std::vector<std::pair<double, double>> get_scp_939_path(std::pair<double, double
         std::pair<double, double> first_point = center_path[i];
         std::pair<double, double> second_point = center_path[i + 1];
 
-        int num_iterations = (1.0 / SCP_939_SPEED) - 1;
+        int num_iterations = (1.0 / SPEED);
         if (first_point.first != second_point.first)
         {
             double dx = second_point.first - first_point.first;
             for (int i = 0; i < num_iterations; ++i)
             {
-                true_path.push_back({first_point.first + dx * SCP_939_SPEED * i, first_point.second});
+                true_path.push_back({first_point.first + dx * SPEED * i, first_point.second});
             }
         }
         else
@@ -131,18 +141,19 @@ std::vector<std::pair<double, double>> get_scp_939_path(std::pair<double, double
             double dy = second_point.second - first_point.second;
             for (int i = 0; i < num_iterations; ++i)
             {
-                true_path.push_back({first_point.first, first_point.second + dy * SCP_939_SPEED * i});
+                true_path.push_back({first_point.first, first_point.second + dy * SPEED * i});
             }
         }
     }
     true_path.push_back(center_path.back());
     std::reverse(true_path.begin(), true_path.end());
+    true_path.pop_back();
     return true_path;
 }
 
 void set_path_to_player(ent::SCP_939 *scp, Gamedata *gamedata)
 {
-    scp->next_path_ = get_scp_939_path(scp->get_pos(), gamedata->get_player()->get_pos(), gamedata);
+    scp->next_path_ = get_scp_939_path(scp->get_pos(), gamedata->get_player()->get_pos(), gamedata, SCP_939_SPEED_PLAYER);
 }
 
 void set_random_path(ent::SCP_939 *scp, Gamedata *gamedata)
@@ -156,11 +167,11 @@ void set_random_path(ent::SCP_939 *scp, Gamedata *gamedata)
         x = std::rand() % width;
         y = std::rand() % height;
     }
-
-    scp->next_path_ = get_scp_939_path(scp->get_pos(), {(double)x + 0.5, (double)y + 0.5}, gamedata);
+    scp->next_path_ = get_scp_939_path(scp->get_pos(), {(double)x + 0.5, (double)y + 0.5}, gamedata, SCP_939_SPEED_RANDOM);
+    
 }
 
-void set_next_pos(ent::SCP_939 *scp, std::vector<std::pair<double, double>> next_path)
+void set_next_pos(ent::SCP_939 *scp, std::vector<std::pair<double, double>> &next_path)
 {
     scp->set_pos(next_path.back());
     next_path.pop_back();
