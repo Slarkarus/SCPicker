383e212736f64596d6a0b3ad74c5a80611b3993e
Евгений Мартьянов <martianov.em@phystech.edu>
2025-08-24 20:00:43 +0300

    Add variative speed for scp_939

--- include/constants.hpp
+++ include/constants.hpp
@@ -10,6 +10,8 @@ const std::string MAP_FILENAME = "original_game_map.scpac";
 
 const double SCP_939_HEARING_DISTANCE = 5.0;
 
-const double SCP_939_SPEED = 1.0 / 20.0;
+const double SCP_939_SPEED_PLAYER = 1.0 / 20.0;
+
+const double SCP_939_SPEED_RANDOM = 1.0 / 64.0;
 
 #endif // CONSTANTS_HPP
