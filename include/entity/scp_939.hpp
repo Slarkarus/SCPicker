3bb09fbb64d4591ae5f59ee6991360684b73035d
Slarkarus <slarkarus@gmail.com>
2025-08-24 18:18:45 +0300

    add interfaces for scp_939 logic

--- include/entity/scp_939.hpp
+++ include/entity/scp_939.hpp
@@ -3,15 +3,20 @@
 
 #include "gamedata.hpp"
 #include "entity/entity.hpp"
+#include <vector>
+#include <utility>
 
 namespace ent
 {
     class SCP_939 : public Entity
-    {
+    {        
     public:
+        std::vector <std::pair<double, double>> next_path_; 
+        
         using Entity::Entity;
 
         void step(Gamedata *gamedata);
+
     };
 };
 
--- src/entity/scp_939.cpp
+++ src/entity/scp_939.cpp
@@ -1,4 +1,33 @@
 #include "gamedata.hpp"
 #include "entity/scp_939.hpp"
 
-void ent::SCP_939::step(Gamedata *gamedata) {}
+bool player_is_near(ent::SCP_939 *scp, Gamedata *gamedata)
+{
+    return true;
+}
+bool able_to_set_path(ent::SCP_939 *scp)
+{
+    return false;
+}
+void set_path_to_player(ent::SCP_939 *scp, Gamedata *gamedata) {}
+void set_random_path(ent::SCP_939 *scp, Gamedata *gamedata) {}
+void set_pos(ent::SCP_939 *scp, std::vector<std::pair<double, double>> &next_path) {}
+
+void ent::SCP_939::step(Gamedata *gamedata)
+{
+    if (!next_path_.empty())
+    {
+        set_pos(this, next_path_);
+    }
+    if (able_to_set_path(this)) // можно поворачивать только в центрах клеток
+    {
+        if (player_is_near(this, gamedata))
+        {
+            set_path_to_player(this, gamedata);
+        }
+        else if (next_path_.empty())
+        {
+            set_random_path(this, gamedata);
+        }
+    }
+}
